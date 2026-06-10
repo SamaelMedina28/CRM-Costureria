6337c95829ba2ed64665a22c3aabe6d5e610eb80
Samael Medina <samaelortiz2218@gmail.com>
2026-06-10 13:23:09 -0700

    feat: se agrego el modelo de clientes

---
+++ src/models/Client.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const clientSchema = new Schema({
+    name:
+    {
+        type: String,
+        required: true
+    },
+    reference:
+    { 
+        type: String
+    },
+    phone: {
+        type: String,
+        maxLength: 15
+    },
+    rating: {
+        type: Number,
+        enum: [1, 2, 3, 4, 5],
+        default: 1
+    }
+    // TODO: Falta por agregar la relacion con los pedidos
+}, {
+    timestamps: true,
+    collection: "clients"
+})
+
+export default model("Client", clientSchema);
