d366c238a59d75a2bb05e2bbadab985d6badaf38
Samael Medina <samaelortiz2218@gmail.com>
2026-06-10 13:42:41 -0700

    feat: crear modelo de telas

---
+++ src/models/Fabric.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const fabricSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true,
+    collection: "fabrics"
+});
+
+export default model("Fabric", fabricSchema);
