9121040af3cf6ffa1652e6f1a7f3bb821d21eb84
Samael Medina <samaelortiz2218@gmail.com>
2026-06-10 13:53:18 -0700

    feat: crear modelo de pedido y relacion con telas y con clientes

---
+++ src/models/Order.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+
+const orderSchema = new Schema({
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: "Client",
+    required: true,
+  },
+  fabricsIHave: [
+    {
+      fabric: {
+        type: Schema.Types.ObjectId,
+        ref: "Fabric"
+      },
+      quantity: { // TODO: cuando se complete como completado el pedido se tienen que restar la cantidad de tela que se uso en el stock
+        type: Number,
+        required: true,
+        min: 0
+      }
+    }
+  ],
+  fabricsINeed: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      color: {
+        type: String,
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  status: {
+    type: String,
+    enum: ["pending", "delievered", "cancelled"],
+    default: "pending",
+  },
+  
+});
