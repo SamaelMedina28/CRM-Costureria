9cc184500cd77415bf5546bf7633fc9bd0690dcb
Samael Medina <samaelortiz2218@gmail.com>
2026-06-10 20:18:20 -0700

    feat: implementar funcion de index para telas

--- src/controllers/fabrics.controller.ts
+++ src/controllers/fabrics.controller.ts
@@ -1,9 +1,14 @@
 import type { Request, Response } from "express";
 import Fabric from "../models/Fabric.js";
 
-export const createFabric = async (req: Request, res: Response) => {
+export const create = async (req: Request, res: Response) => {
   const { name, price, stock, color, type } = req.body;
   const fabric = new Fabric({ name, price, stock, color, type, user: req.user?.id });
   await fabric.save();
   res.status(201).json({ message: "Tela creada correctamente", fabric });
 };
+
+export const getAll = async (req: Request, res: Response) => {
+  const fabrics = await Fabric.find({ user: req.user!.id }).select("-__v").sort({ createdAt: -1 });
+  res.status(200).json(fabrics);
+}
--- src/routes/fabric.routes.ts
+++ src/routes/fabric.routes.ts
@@ -2,11 +2,11 @@ import express from "express";
 import type { Request, Response } from "express";
 import { createFabricRules } from "../validations/fabric.validations.js";
 import { validate } from "../middlewares/validate.js";
-import { createFabric } from "../controllers/fabrics.controller.js";
+import { create, getAll } from "../controllers/fabrics.controller.js";
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {});
-router.post("/", createFabricRules, validate, createFabric);
+router.get("/", getAll); 
+router.post("/", createFabricRules, validate, create);
 router.put("/:id", (req: Request, res: Response) => {});
 router.delete("/:id", (req: Request, res: Response) => {});
 
