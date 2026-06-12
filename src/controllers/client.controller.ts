7bbfb0ec755b1ac86894320b1580fce8d8b3232a
Samael Medina <samaelortiz2218@gmail.com>
2026-06-11 22:45:48 -0700

    feat: funcionalidad de ver todos los clientes

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -1,6 +1,21 @@
 import type { Request, Response } from "express";
 import Client from "../models/Client.js";
 
+export const getAll = async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const clients = await Client.find({ user: userId }).sort({ name: "asc" });
+  return res.status(200).json(clients);
+};
+
+export const getById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const client = await Client.findById(id);
+  if (!client) {
+    return res.status(404).json({ message: "Cliente no encontrado" });
+  }
+  return res.status(200).json(client);
+};
+
 export const create = async (req: Request, res: Response) => {
   const userId = req.user!.id;
   const { name, phone, reference, rating } = req.body;
@@ -11,5 +26,5 @@ export const create = async (req: Request, res: Response) => {
     rating,
     user: userId,
   });
-  return res.status(201).json({ message: "Client created successfully", client });
+  return res.status(201).json({ message: "Cliente creado exitosamente", client });
 };
--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,9 +1,10 @@
-import { create } from "../controllers/client.controller.js";
+import { create, getAll } from "../controllers/client.controller.js";
 import { createClientRules } from "../validations/client.validations.js";
 import { validate } from "../middlewares/validate.js";
 import express from "express";
 const router = express.Router();
 
+router.get("/", getAll);
 router.post("/", createClientRules, validate, create);
 
 export default router;
