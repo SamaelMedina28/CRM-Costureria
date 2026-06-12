7413e463921fec2bc0441dd5f26efd1425de3183
Samael Medina <samaelortiz2218@gmail.com>
2026-06-11 22:49:45 -0700

    feat: agregar ruta en clientes de getById

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,10 +1,11 @@
-import { create, getAll } from "../controllers/client.controller.js";
+import { create, getAll, getById } from "../controllers/client.controller.js";
 import { createClientRules } from "../validations/client.validations.js";
 import { validate } from "../middlewares/validate.js";
 import express from "express";
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/:id", getById);
 router.post("/", createClientRules, validate, create);
 
 export default router;
