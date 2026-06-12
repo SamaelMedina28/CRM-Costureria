3243995cebff106b1a859f78d6387d8141c907c6
Samael Medina <samaelortiz2218@gmail.com>
2026-06-12 13:52:29 -0700

    feat: funcionalidad de ver todos los pedidos

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,8 +1,16 @@
 import type { Request, Response } from "express";
 import Order from "../models/Order.js";
 
+export const getAll = async (req: Request, res: Response) => {
+  const { id } = req.user!;
+  const orders = await Order.find({ user: id })
+    .populate("client", "name")
+    .populate("fabricsIHave.fabric", "name")
+  res.status(200).json({ orders });
+}
+
 export const create = async (req: Request, res: Response) => {
-  try {
+
     const { client, fabricsIHave, fabricsINeed, status } = req.body;
     const order = new Order({
       user: req.user!.id,
@@ -12,11 +20,6 @@ export const create = async (req: Request, res: Response) => {
       status,
     });
     await order.save();
-    res.status(201).json({ ok: true, data: order });
-  } catch (error) {
-    if(error instanceof Error) {
-      return res.status(400).json({ ok: false, error: error.message });
-    }
-    res.status(500).json({ ok: false, error: "Error al crear la orden" });
-  }
+    res.status(201).json({ order, message: "Orden creada correctamente" });
+
 }
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createOrderValidations } from "../validations/order.validations.js"
 import { validate } from "../middlewares/validate.js";
-import { create } from "../controllers/order.controller.js";
+import { create, getAll } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
+router.get("/", getAll)
 router.post("/", createOrderValidations, validate, create)
   
 export default router;
