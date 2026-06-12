034e1cd7fb1402f682e6e2c85fbec69826f1369e
Samael Medina <samaelortiz2218@gmail.com>
2026-06-11 22:13:01 -0700

    feat: crear reglas de validacion para crear un cliente

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,33 +1,18 @@
 import { Schema, model } from "mongoose";
 
-const clientSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    name:
+const clientSchema = new Schema(
     {
-        type: String,
-        required: true
+        user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        name: { type: String, required: true },
+        reference: { type: String },
+        phone: { type: String, maxLength: 15 },
+        rating: { type: Number, enum: [1, 2, 3, 4, 5], default: 1 },
+        // TODO: Falta por agregar la relacion con los pedidos
     },
-    reference:
-    { 
-        type: String
+    {
+        timestamps: true,
+        collection: "clients",
     },
-    phone: {
-        type: String,
-        maxLength: 15
-    },
-    rating: {
-        type: Number,
-        enum: [1, 2, 3, 4, 5],
-        default: 1
-    }
-    // TODO: Falta por agregar la relacion con los pedidos
-}, {
-    timestamps: true,
-    collection: "clients"
-})
+);
 
 export default model("Client", clientSchema);
